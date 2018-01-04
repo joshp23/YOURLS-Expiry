41f54917e69d1917016cc591b299aa7b6218de8d
Joshua P Panter <joshu@unfettered.net>
2018-01-03 20:15:05 -0500

    update for PDO compliance

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
 # YOURLS-Expiry
 YOURLS plugin to define conditions under which links will expire - time and click limited links
 
-### Work in progress.
-
-#### Concept:
-Granular expiration rules for short URL's in YOURLS
-
 ##### What's Working:
 -  add per-link and/or global time limited links
 -  add per-link and/or global click count limited links
 -  update links already in database to have expiration conditions
 -  upon shorturl expiration:
     - delete from YOURLS 
-    - update short url's with a per link or global secondary URL
+    - update short url's with an optional per link or global secondary URL
 -  robust admin area features
    - optionally highlight expiry links in admin interface
    - provide page to list all expiry links with details (time left, etc)
@@ -27,9 +22,9 @@ Granular expiration rules for short URL's in YOURLS
 -  Integrate expiry data to stats page OR stand alone expiry stats page.
 -  more clarity of info in list page (human readable time left, # of clicks left)
 -  simple and robust options for updating expiry data via the list page (add X clicks or days to expiry, etc...)
--  api to call for expiry data and update links (see note)
+-  api to call for expiry data
 
 ##### What's not working
--  admin interface to add a link with expiry data (some js included... not my strong suit)
+-  admin interface to add a link with expiry data (some js included... WIP)
 ###### Note: 
- When in use with the [Change Error Msgs](https://github.com/adigitalife/yourls-change-error-messages) plugin, a simple action=shorturl call to any link in the database will trigger an update if sent with expiry info.
+ Uses code from the [Change Error Msgs](https://github.com/adigitalife/yourls-change-error-messages) plugin so that a simple action=shorturl call to any link in the database will trigger an update if this request is sent with expiry info. 
--- expiry/plugin.php
+++ expiry/plugin.php
@@ -3,13 +3,12 @@
 Plugin Name: Expiry
 Plugin URI: https://github.com/joshp23/YOURLS-Expiry
 Description: Will set expiration conditions on your links (or not)
-Version: 0.10.0
+Version: 0.11.1
 Author: Josh Panter
 Author URI: https://unfettered.net
 */
 // TODO expiry data in stats page
 // TODO fix the fine js
-// FIXME api works great, public interface not so much
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 /*
@@ -305,7 +304,12 @@ echo <<<HTML
 				</tr>
 HTML;
 	// populate table rows with expiry data if there is any
-	$expiry_list = $ydb->get_results("SELECT * FROM `expiry` ORDER BY timestamp DESC");
+
+	$table = 'expiry';
+	$sql = "SELECT * FROM $table ORDER BY timestamp DESC";
+	$binds = array('keyword' => $keyword);
+	$expiry_list = $ydb->fetchAll($sql, $binds);
+
 	if($expiry_list) {
 		foreach( $expiry_list as $expiry ) {
 			$kword  = $expiry->keyword;
@@ -358,7 +362,11 @@ function show_expiry_tablerow($row, $keyword, $url, $title, $ip, $clicks, $times
 	if($expiry_expose !== "false") {
 
 		// If the keyword is set to expire, make the URL show in green;
-		$expiry = $ydb->get_row("SELECT * FROM `expiry` WHERE BINARY `keyword` = '$keyword'");
+
+		$table = 'expiry';
+		$sql = "SELECT * FROM $table WHERE BINARY `keyword` = :keyword";
+		$binds = array('keyword' => $keyword);
+		$expiry = $ydb->fetchOne($sql, $binds);
 	
 		if( $expiry ) {
 			$old_key = '/td class="keyword"/';
@@ -472,10 +480,12 @@ yourls_add_action( 'redirect_shorturl', 'expiry_check' );
 function expiry_check( $args ) {
 
 	global $ydb;
-	
-        $keyword = $args[1]; // Keyword for this request
 
-	$expiry = $ydb->get_row("SELECT * FROM `expiry` WHERE `keyword` = '$keyword'");
+    $keyword = $args[1]; // Keyword for this request
+	$table = 'expiry';
+	$sql = "SELECT * FROM $table WHERE `keyword` = :keyword";
+	$binds = array('keyword' => $keyword);
+	$expiry = $ydb->fetchOne($sql, $binds);
 	
 	if( $expiry ) {
 	
@@ -1065,4 +1075,33 @@ function expiry_cleanup( $args ) {
 	// Delete the expiry data, no need for it anymore
 	$ydb->query("DELETE FROM `expiry` WHERE `keyword` = '$keyword';");
 
+}
+
+// Change-Error MSG behavior
+if((yourls_is_active_plugin('change-error-messages/plugin.php')) == false) {
+
+	yourls_add_filter( 'add_new_link', 'change_error_messages' );
+	// If the keyword exists, display the long URL in the error message
+	function change_error_messages( $return, $url, $keyword, $title  ) {
+		if ( isset( $return['code'] ) ) {
+			if ( $return['code'] === 'error:keyword' ){
+				$long_url = yourls_get_keyword_longurl( $keyword );
+				if ($long_url){
+					$return['message']	= 'The keyword "' . $keyword . '" already exists for: ' . $long_url;
+				} elseif ( yourls_keyword_is_reserved( $keyword ) ){
+								$return['message']	= "The keyword '" . $keyword . "' is reserved";
+				}
+			}
+			elseif ( $return['code'] === 'error:url' ){
+				if ($url_exists = yourls_url_exists( $url )){
+					$keyword = $url_exists->keyword;
+					$return['status']   = 'success';
+					$return['message']	= 'This URL already has a short link: ' . YOURLS_SITE .'/'. $keyword;
+					$return['title']    = $url_exists->title;
+					$return['shorturl'] = YOURLS_SITE .'/'. $keyword;
+				}
+			}
+		}
+		return yourls_apply_filter( 'after_custom_error_message', $return, $url, $keyword, $title );
+	}
 }
