782ad0829c8ce24cfefa5ff88b7f761c986259dc
Joshua P Panter <joshu@unfettered.net>
2020-03-01 01:37:02 -0500

    version bump

--- expiry/plugin.php
+++ expiry/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Expiry
 Plugin URI: https://github.com/joshp23/YOURLS-Expiry
 Description: Will set expiration conditions on your links (or not)
-Version: 2.1.1
+Version: 2.1.2
 Author: Josh Panter
 Author URI: https://unfettered.net
 */
